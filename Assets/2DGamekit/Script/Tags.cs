10a67127969a51496ab78957597478ac04dbbe5a
fjeminjohnson <fjfleminjohnson@gmail.com>
2020-02-28 15:56:06 +0530

    Modified version
    This branch is not reviewd yet

---
+++ Assets/2DGamekit/Script/Tags.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags
+{
+    public static string HorizontalSpeed = "HorizontalSpeed";
+    public static string VerticalSpeed = "VerticalSpeed";
+    public static string TimeOut = "TimeOut";
+    public static string Crouching = "Crouching";
+    public static string Grounded = "Grounded";
+    public static string Pushing = "Pushing";
+    public static string Hurt = "Hurt";
+    public static string Dead = "Dead";
+    public static string MeleeAttack = "MeleeAttack";
+    public static string Respawn = "Respawn";
+    public static string HoldingGun = "HoldingGun";
+    public static string ForcedRespawn = "ForcedRespawn";
+    public static string Horizontal = "Horizontal";
+    public static string Vertical = "Vertical";
+    public static string Jump = "Jump";
+}
