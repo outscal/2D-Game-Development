a0d6a628e7bdb29e9b3ddd195962b1bb2ec35ddf
Shikhar <shikhar.kr007@gmail.com>
2020-03-10 23:48:49 +0530

    score and level reset and lobby scene added

--- Assets/Scripts/LevelController.cs
+++ Assets/Scripts/LevelController.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour {
     public GameObject levelComplteUI;
+    public int score;
+    public Text scoreText;
     void Start () {
+        GamaeData.SCORE = 0;
+        score = GamaeData.SCORE;
+        scoreText.text = "Score: " + score;
         levelComplteUI.SetActive (false);
+        Debug.Log ("enum = " + score);
     }
 
     private void OnCollisionEnter2D (Collision2D other) {
@@ -13,6 +20,11 @@ public class LevelController : MonoBehaviour {
             levelComplteUI.SetActive (true);
             Time.timeScale = 0;
         }
+    }
+
+    void Update () {
+        score = GamaeData.SCORE;
+        scoreText.text = "Score: " + score;
     }
 
 }
---
+++ Assets/Scripts/enums.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GamaeData {
+    public static int SCORE = 0;
+
+}
+
+public class enums : MonoBehaviour {
+
+    public void LoadGameScene () {
+        SceneManager.LoadScene ("2dPlatformer");
+    }
+}
