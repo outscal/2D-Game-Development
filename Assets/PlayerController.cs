b76e62670347399153cbbafc8da799b79f8d9215
Abhishek-D-Developer <61371395+Abhishek-D-Developer@users.noreply.github.com>
2020-03-11 13:37:01 +0530

    feature1-Animations
    Added animations like idle, walk, run, crouch.

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public Animator animator;
+
+	/*private void Awake()
+	{
+		Debug.Log("Player controller Awake");
+	}
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Collision: "+collision.gameObject.name);
+	}*/
+
+	private void Update()
+	{
+		float speed = Input.GetAxisRaw("Horizontal");
+
+		animator.SetFloat("Speed", Mathf.Abs(speed));
+
+		Vector3 scale = transform.localScale;
+		if(speed < 0){
+			scale.x = -1f * Mathf.Abs(scale.x);
+		} else if(speed > 0){
+			scale.x = Mathf.Abs(scale.x);
+		}
+		transform.localScale = scale;
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			animator.SetBool("Jump", true);
+		}else{
+			animator.SetBool("Jump", false);
+		}
+
+		if(Input.GetKeyDown(KeyCode.C)){    // I made a new Input button in input manager;
+			animator.SetBool("Crouch", true);
+		}else{
+			animator.SetBool("Crouch", false);
+		}
+
+		if(Input.GetKeyDown(KeyCode.LeftShift)){
+			animator.SetBool("Run", true);
+		}else{
+			animator.SetBool("Run", false);
+		}
+	}
+}
