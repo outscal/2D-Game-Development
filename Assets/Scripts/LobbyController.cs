d66aa94e0e6ea56ba3826eca620e377d521beacf
akankshreddy <chinnu.akanksh@gmail.com>
2020-03-14 12:07:00 +0530

    temp123

---
+++ Assets/Scripts/LobbyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LobbyController : MonoBehaviour
+{
+
+    public Button buttonPlay;
+    // Start is called before the first frame update
+
+    private void Awake()
+    {
+        buttonPlay.onClick.AddListener(PlayGame);
+    }
+
+    // Update is called once per frame
+    private void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
