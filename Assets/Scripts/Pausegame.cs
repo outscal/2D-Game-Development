a28f96920e5d123c2f07dfb9b90611d2a3f5d975
Pranav Mahendra <>
2020-03-23 00:11:38 +0530

    Added a MainMenu along with script to switch level.

---
+++ Assets/Scripts/Pausegame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausegame : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.P))
+        {
+            Time.timeScale = 0;
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
