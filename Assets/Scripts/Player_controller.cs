ec386c586be497bd0b7a1c9946e337a0e3270543
disaz <diveshc76@gmail.com>
2022-01-22 23:48:20 +0530

    Feature 3

--- Assets/Scripts/Player_controller.cs
+++ Assets/Scripts/Player_controller.cs
@@ -4,31 +4,58 @@ using UnityEngine;
 
 public class Player_controller : MonoBehaviour
 {
-    public Animator animator;  
-   private void Awake ()
+    public Animator animator;
+
+    public float speed;
+    private Rigidbody2D rb2D;
+    private void Awake()
     {
         Debug.Log("Player controller awake");
+        rb2D = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+    
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+
+        MoveCharacter(horizontal);
+
+        PlayMovementAnimation(horizontal);
+
+
+
+    }
+    private void MoveCharacter(float horizontal)
+    {
+
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
 
     }
 
-    private void update ()
+
+    private void PlayMovementAnimation(float horizontal)
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
-        if (speed < 0)
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+
+        Vector3 scale = transform.localScale;
+        if (horizontal < 0)
         {
-            Vector3 scale = transform.localScale;
-            if (speed <0 ) { 
             scale.x = -1f * Mathf.Abs(scale.x);
-        } else if (speed > 0)
+
+        }
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
 
         }
         transform.localScale = scale;
 
-
+        Input.GetAxisRaw("Vertical");
+        Input.GetKeyDown(KeyCode.Space);
     }
-
-    
 }
