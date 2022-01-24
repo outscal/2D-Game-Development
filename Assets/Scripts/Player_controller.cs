86cf61b9b833e94c5901122df3029fb53663341b
disaz <diveshc76@gmail.com>
2022-01-24 14:31:14 +0530

    Feature_3

--- Assets/Scripts/Player_controller.cs
+++ Assets/Scripts/Player_controller.cs
@@ -7,37 +7,46 @@ public class Player_controller : MonoBehaviour
     public Animator animator;
 
     public float speed;
-    private Rigidbody2D rb2D;
+    public float jump;
+    
+    private Rigidbody2D rb2d;
     private void Awake()
     {
         Debug.Log("Player controller awake");
-        rb2D = gameObject.GetComponent<Rigidbody2D>();
-
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        SpriteRenderer sprite = gameObject.GetComponent<SpriteRenderer>();      
     }
     
 
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
+        float  vertical = Input.GetAxisRaw("Vertical");
 
-        MoveCharacter(horizontal);
+        MoveCharacter(horizontal, vertical ); 
 
-        PlayMovementAnimation(horizontal);
+        PlayMovementAnimation(horizontal,vertical);
 
 
-
+ 
     }
-    private void MoveCharacter(float horizontal)
+    private void MoveCharacter(float horizontal, float vertical )
     {
 
         Vector3 position = transform.position;
         position.x += horizontal * speed * Time.deltaTime;
         transform.position = position;
 
+
+        if (vertical > 0)
+        {
+           rb2d.AddForce(new Vector2(0f,jump),ForceMode2D.Force);
+        }
+
     }
 
 
-    private void PlayMovementAnimation(float horizontal)
+    private void PlayMovementAnimation(float horizontal , float vertical)
     {
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
@@ -55,7 +64,28 @@ public class Player_controller : MonoBehaviour
         }
         transform.localScale = scale;
 
-        Input.GetAxisRaw("Vertical");
-        Input.GetKeyDown(KeyCode.Space);
+        // Input.GetKeyDown(KeyCode.Space);
+        if ( vertical > 0)
+        {
+        animator.SetBool("Jump" , true );
+        } else
+        {
+            animator.SetBool("Jump", false);
+        }
     }
+    // private bool IsGrounded(){
+    //       float extraHeightText = .01f;
+    //         RaycastHit2D RaycastHit = Physics2D.Raycast(boxCollider2d.bounds.center, Vector3.down, boxCollider2d.bounds.extents.y + extraHeightText);  
+    //         Color rayColor; 
+    //                if (raycastHit.collider !=null) {
+    //                    rayColor = Color.green;
+
+    //         }
+    //         else {
+    //             rayColor = Color.red;
+    //         }
+    //         Debug.DrawRay(boxCollider2d.bounds.center,Vector3.down * (boxCollider2d.bounds.extents.y + extraHeightText));
+    //      return raycastHit.collider != null;0
+    //     }
+
 }
