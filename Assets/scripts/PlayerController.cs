8a8dc2e72dc9dfee627f50d665645514a4bb2314
manojkumar4799 <manoj.mutukuloju18@gmail.com>
2022-02-20 02:26:43 +0530

    created crouch animation and script for animations

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator playerAnimator;
+    void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    
+    void Update()
+    {
+        PlayerRunAnimation();
+
+        PlayerCrouchAnimation();
+
+    }
+
+    private void PlayerCrouchAnimation()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            playerAnimator.SetBool("crouch", true);
+        }
+        if (Input.GetKeyUp(KeyCode.C))
+        {
+            playerAnimator.SetBool("crouch", false);
+        }
+    }
+
+    private void PlayerRunAnimation()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            playerAnimator.SetFloat("speed", 0.6f);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            playerAnimator.SetFloat("speed", 0f);
+        }
+    }
+}
