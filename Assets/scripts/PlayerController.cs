2906d1229209a99eaf1890749c4ae0b53807932c
manojkumar4799 <manoj.mutukuloju18@gmail.com>
2022-02-25 22:22:35 +0530

    movement code lines added and jump also include

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,9 +7,14 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] Animator playerAnimator;
     [SerializeField] BoxCollider2D fullCollider;
-    [SerializeField] Rigidbody2D rb;
-
+    public Rigidbody2D rb;
+    public float speed;
     [SerializeField] float playerJump;
+
+    [SerializeField] LayerMask groundlayer;
+    [SerializeField] Transform groundcheckTansform;
+    public float circleRadius;
+    [SerializeField] bool isGrounded;
    
 
 
@@ -17,28 +22,25 @@ public class PlayerController : MonoBehaviour
     {
         playerAnimator = GetComponent<Animator>();
         fullCollider = GetComponent<BoxCollider2D>();
-        rb = GetComponent<Rigidbody2D>();
-       
+        rb = GetComponent<Rigidbody2D>();       
     }
 
     
     void Update()
     {
-
+        isGrounded = Physics2D.OverlapCircle(groundcheckTansform.position, circleRadius, groundlayer);
         RunPlayer();
         PlayerCrouch();
         PlayerJump();
-
     }
 
     private void PlayerJump()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space)&&isGrounded)
         {
             playerAnimator.SetTrigger("jump");
             rb.velocity = new Vector2(0, playerJump);
-        }
-        
+        }        
     }
 
     private void PlayerCrouch()
@@ -59,18 +61,21 @@ public class PlayerController : MonoBehaviour
 
     private void RunPlayer()
     {
-        float playerSpeed = Input.GetAxisRaw("Horizontal");
+        float playerXmovement = Input.GetAxisRaw("Horizontal");
+        Vector2 playerPos = transform.position;
+        playerPos.x = playerPos.x + playerXmovement * speed * Time.deltaTime;
+        transform.position = playerPos;
         Vector3 scale = transform.localScale;
-        playerAnimator.SetFloat("speed", Mathf.Abs(playerSpeed));
+        playerAnimator.SetFloat("speed", Mathf.Abs(playerXmovement));
         playerAnimator.SetBool("jump", false);
 
         
         
-        if (playerSpeed < 0)
+        if (playerXmovement < 0)
         {
             transform.localRotation = Quaternion.Euler(0, 180, 0);
-        }
-        if (playerSpeed > 0)
+        } 
+        if (playerXmovement > 0)
         {
             transform.localRotation = Quaternion.Euler(0, 0, 0);
         }
