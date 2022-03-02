8b610e29d7cf830f2b6a929abd70b1a6b9be5024
DESKTOP-EINK3HP\kurun <komalkuru@gmail.com>
2022-03-02 22:00:31 +0530

    Implemented crouch animation

---
+++ Assets/Scripts/CrouchMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using playerMovement;
+
+public class CrouchMovement : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private float crouch;
+    [SerializeField] private BoxCollider2D b_idle;
+    
+    private void Awake()
+    {
+        SpriteRenderer sprite = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        }
+
+    private void Update()
+    {
+       float vertical = Input.GetAxisRaw("Vertical") ;
+       PlayerCrouchMovement(vertical);
+    }
+
+    void PlayerCrouchMovement(float vertical) {
+        if(vertical > 0)
+        {
+            animator.SetBool("Crouch", true);
+            rb.AddForce(new Vector2(0f, crouch), ForceMode2D.Force);
+        }
+        else {
+            animator.SetBool("Crouch", false);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,73 +2,75 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+namespace playerMovement 
 {
-    Animator animator;
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private float speed;
-    [SerializeField] private float jump;
-
-    private void Awake()
+    public class PlayerController : MonoBehaviour 
     {
-        Debug.Log("Player controller awake");
-        rb = gameObject.GetComponent<Rigidbody2D>();
-        SpriteRenderer sprite = gameObject.GetComponent<SpriteRenderer>();
-    }
+        Animator animator;
+        [SerializeField] private Rigidbody2D rb;
+        [SerializeField] private float speed;
+        [SerializeField] private float jump;
+
+        private void Awake()
+        {
+            Debug.Log("Player controller awake");
+            rb = gameObject.GetComponent<Rigidbody2D>();
+            SpriteRenderer sprite = gameObject.GetComponent<SpriteRenderer>();
+        }
 
         private void Start() {
             rb = GetComponent<Rigidbody2D>();
             animator = GetComponent<Animator>();
         }
 
-    private void Update() {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        private void Update() {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-        PlayerMoving(horizontal, vertical);
-        PlayerMovememtAnimation(horizontal, vertical);
-    }
-
-    void PlayerMoving(float horizontal, float vertical) {
-        //Move character horizontally
-        Vector3 position = transform.position;
-
-        // speed = distance / time;      timr.deltaTime = 1 / Frames per second
-        position.x = position.x + horizontal * speed * Time.deltaTime;
-        transform.position = position; 
-
-        //Move character vertically
-       if(vertical > 0) {
-            animator.SetBool("Jump", true);
-            rb.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
-        }
-        else {
-            animator.SetBool("Jump",false);
+            PlayerMoving(horizontal, vertical);
+            PlayerMovementAnimation(horizontal, vertical);
         }
 
-    }
+        void PlayerMoving(float horizontal, float vertical) {
+            //Move character horizontally
+            Vector3 position = transform.position;
 
-    void PlayerMovememtAnimation(float horizontal, float vertical) {
-        animator.SetFloat("Speed", Mathf.Abs(horizontal));
-        
-        Vector3 scale = transform.localScale;
-        
-        if(horizontal < 0) {
-            scale.x = -1f * Mathf.Abs(scale.x);
-        } 
-        else if(horizontal > 0) {
-            scale.x = Mathf.Abs(scale.x);
-        }
-        transform.localScale = scale;
+            // speed = distance / time;      time.deltaTime = 1 / Frames per second
+            position.x = position.x + horizontal * speed * Time.deltaTime;
+            transform.position = position; 
 
-        //Jump
-
+            //Move character vertically
         if(vertical > 0) {
-            animator.SetBool("Jump", true);
+                animator.SetBool("Jump", true);
+                rb.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+            }
+            else {
+                animator.SetBool("Jump",false);
+            }
+
         }
-        else {
-            animator.SetBool("Jump", false);
+
+        void PlayerMovementAnimation(float horizontal, float vertical) {
+            animator.SetFloat("Speed", Mathf.Abs(horizontal));
+            
+            Vector3 scale = transform.localScale;
+            
+            if(horizontal < 0) {
+                scale.x = -1f * Mathf.Abs(scale.x);
+            } 
+            else if(horizontal > 0) {
+                scale.x = Mathf.Abs(scale.x);
+            }
+            transform.localScale = scale;
+
+            //Jump
+
+            if(vertical > 0) {
+                animator.SetBool("Jump", true);
+            }
+            else {
+                animator.SetBool("Jump", false);
+            }
         }
     }
-
 }
